bafa0a3b51fbd3b957d2bcdeedd3f2a6288ed42b
supriyo34 <supriyodhar35@gmail.com>
2026-08-02 18:00:37 +0530

    deployment changes

--- EmployeeAPI/Program.cs
+++ EmployeeAPI/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<EmployeeDbContext>(options =>
         builder.Configuration.GetConnectionString("EmployeeDb")));
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
