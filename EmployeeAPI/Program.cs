2f6df3cf43865fccb028dfa49d3958a04dee054f
supriyo34 <supriyodhar35@gmail.com>
2026-08-04 23:57:51 +0530

    kubernetes

--- EmployeeAPI/Program.cs
+++ EmployeeAPI/Program.cs
@@ -7,21 +7,30 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<EmployeeDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("EmployeeDb")));
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+//
+// Automatically apply migrations
+//
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<EmployeeDbContext>();
+
+    db.Database.Migrate();
+}
 
 app.Run();
